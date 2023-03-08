1f6ba7a8e0f8addd20c4033e71f5bce57df07010
PARK9898 <wjddjs21@naver.com>
2023-03-08 21:53:47 +0900

    problem 25206

---
+++ Algorithm/Problem25206.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+
+public class Problem25206 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		double TotalScoreAndTime = 0;
+		List<Subject> subjects = new ArrayList<Subject>();
+		for(int i = 0 ; i <20 ; i++) {
+
+
+		}
+	}
+
+	public class Subject{
+	String Subject_name;
+	double Score;
+	double Time;
+	double curScoreTime;
+	}
+
+
+
+}
