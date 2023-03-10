1172345799638250dca757e4b92179c1d4b6d3c3
PARK9898 <wjddjs21@naver.com>
2023-03-10 20:40:24 +0900

    problem 25206

--- Algorithm/Problem25206.java
+++ Algorithm/Problem25206.java
@@ -7,21 +7,78 @@ public class Problem25206 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 
-		double TotalScoreAndTime = 0;
 		List<Subject> subjects = new ArrayList<Subject>();
-		for(int i = 0 ; i <20 ; i++) {
 
+		for(Subject s : subjects) {
+			String a = scanner.next();
+			String[] ch = a.split(" ");
+			subjects.add(new Subject(ch[0],ch[1],Integer.parseInt(ch[2]),0 ));
+		}
 
+		for(Subject s : subjects) {
+			String str = s.getScore();
+			switch (str) {
+				case "A+":
+					s.setScoreToDouble(4.5);
+					break;
+				case "A0":
+					s.setScoreToDouble(4.0);
+					break;
+				case "B+":
+					s.setScoreToDouble(3.5);
+					break;
+				case "B0":
+					s.setScoreToDouble(3.0);
+					break;
+				case "C+":
+					s.setScoreToDouble(2.5);
+					break;
+				case "C0":
+					s.setScoreToDouble(2.0);
+					break;
+				case "D+":
+					s.setScoreToDouble(1.5);
+					break;
+				case "D0":
+					s.setScoreToDouble(1.0);
+					break;
+				case "F":
+					s.setScoreToDouble(0.0);
+					break;
+			}
 		}
 	}
 
-	public class Subject{
-	String Subject_name;
-	double Score;
-	double Time;
-	double curScoreTime;
+	public static class Subject{
+		String Subject_name; // 과목명
+		String Score; // 과목 평점
+		double Time; // 학점
+		double ScoreToDouble; //스코어를 더블로 변환
+
+		public String getSubject_name() {
+			return Subject_name;
+		}
+
+		public String getScore() {
+			return Score;
+		}
+
+		public double getTime() {
+			return Time;
+		}
+		public double getScoreToDouble(){
+			return ScoreToDouble;
+		}
+
+		public void setScoreToDouble(double scoreToDouble) {
+			ScoreToDouble = scoreToDouble;
+		}
+
+		public Subject(String subject_name, String score, double time, double scoretodouble) {
+			Subject_name = subject_name;
+			Score = score;
+			Time = time;
+			ScoreToDouble = scoretodouble;
+		}
 	}
-
-
-
 }
