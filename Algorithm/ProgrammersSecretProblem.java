e695b90c6f49f28ab94136d3aada12b060365707
PARK9898 <wjddjs21@naver.com>
2023-09-21 15:25:01 +0900

    "둘만의 암호"

--- Algorithm/ProgrammersSecretProblem.java
+++ Algorithm/ProgrammersSecretProblem.java
@@ -4,18 +4,21 @@ public class ProgrammersSecretProblem {
 		answer= solution("aukks"	,"wbqd",5);
 		System.out.println(answer);
 	}
+	// 조건 1 현재 문자 s.charAt(i) ~ index 까지 검사한 후 만약에 있으면
+	// skip 해주고 다음문자를 넣어주고 아니면 그냥 다음 문자를 넣어준다
 	public static String solution(String s, String skip, int index) {
 		String answer = "";
-
+		// 바꿔줄 비밀문자를 일일이 일단 검사할거임
 		for(int i = 0; i < s.length(); i++) {
 			char c = s.charAt(i);
-			for (int j = 0; j < index; j++) {
+			int j = 0;
+			while (j < index) {
 				c += 1;
 				if (c > 'z') {
 					c -= 26;
 				}
-				if (skip.contains(String.valueOf(c))) {
-					j--;
+				if (!skip.contains(String.valueOf(c))) {
+					j++;
 				}
 			}
 			answer += c;
